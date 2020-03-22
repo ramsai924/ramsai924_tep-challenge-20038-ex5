e8a3e4a9a13ec707310750d3685de70cdbb009c6
ramsai <ramsai924@gmail.com>
2020-03-23 01:15:47 +0530

    async await

--- ex5.js
+++ ex5.js
@@ -1,16 +1,11 @@
 function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
-
-	console.log("Requesting: " + url);
-
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+	 var randomDelay = 1000;
+	                
+	                    console.log("Requesting: " + url);
+	                
+	                    return new Promise(function(resolve,reject){
+				                            setTimeout(resolve,randomDelay);
+				                        })
 }
 
 function output(text) {
@@ -20,8 +15,28 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+	 var fake_responses = {
+		                             "file1": "The first text",
+		                             "file2": "The middle text",
+		                             "file3": "The last text"
+		                         };
+	                
+	                      return fakeAjax(file)
+	                      .then(function(){                    
+				                               console.log(fake_responses[file]);
+				                             })
+	                        .catch(function(err){
+					                            console.log(err)
+					                        })
 }
+  async function syn(){
+	                      await getFile("file1");
+	                      await getFile("file2");
+	                      await getFile("file3");    
+	                  
+	                      await output("complete");
+	                  }
+                syn()
 
 // request an array of files at once in "parallel"
 // ???
